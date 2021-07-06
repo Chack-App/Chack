bc029ffbf3c8a7980fdcd7b5c615e67148159de5
David Degenstein <ddegenstein15@gmail.com>
2021-07-06 17:28:28 -0400

    Created Manual Item Entry Form

--- app/navigation/EventNavigator.js
+++ app/navigation/EventNavigator.js
@@ -1,17 +1,24 @@
 import React from "react";
 import colors from "../config/colors";
-import Events from "../screens/Events";
+import Events from "../screens/EventScreens/Events";
+import ManualItemEntry from "../screens/EventScreens/ManualItemEntry";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 const EventNavigator = () => (
-  <Stack.Navigator initialRouteName="Events">
+  <Stack.Navigator initialRouteName="ManualItemEntry">
     <Stack.Screen
       name="Events"
       component={Events}
       options={{ ...gatewayHeaderStyles, headerTitle: "Events" }}
     />
+    <Stack.Screen
+      name="ManualItemEntry"
+      component={ManualItemEntry}
+      options={{ ...gatewayHeaderStyles, headerTitle: "Manual Item Entry" }}
+    />
+
   </Stack.Navigator>
 );
 
