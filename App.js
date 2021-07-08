63c0b7cc043e3092ffe4667b4256bc92cbdeb481
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-07-08 06:32:48 -0700

    changed Navigator back to AppNavigator

--- App.js
+++ App.js
@@ -23,8 +23,8 @@ export default function App() {
   return (
     <ApolloProvider client={client}>
       <NavigationContainer>
-        <AuthNavigator />
-        {/* <AppNavigator /> */}
+        {/* <AuthNavigator /> */}
+        <AppNavigator />
       </NavigationContainer>
     </ApolloProvider>
   )
