e255f7fe3974201df49c2cd591a137c58462beb7
Andrew Larsen <ajlarsen2@gmail.com>
2021-07-08 09:24:20 -0400

    Query Single Event

--- app/client/queries/eventQueries.js
+++ app/client/queries/eventQueries.js
@@ -1,5 +1,23 @@
 import { gql } from '@apollo/client';
 
+export const GET_EVENT = gql`
+query FetchAnEvent($id: ID!) {
+  event(id: $id) {
+    id
+    eventName
+     description
+    passcode
+    receipts {
+      id
+      name
+      isPaid
+      eventId
+      cardDownId
+    }
+  }
+}
+`
+
 export const CREATE_EVENT = gql`
   mutation CreateAnEvent($eventName: String, $description: String) {
     addEvent(eventName: $eventName, description: $description) {
