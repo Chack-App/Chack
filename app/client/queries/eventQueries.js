dc31f8603edf17f0f953eb3f748c3cb515e0ac6b
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-07-09 09:36:59 -0700

    added state management and setting tokens to Signup Button

--- app/client/queries/eventQueries.js
+++ app/client/queries/eventQueries.js
@@ -1,58 +1,58 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client"
 
 export const GET_EVENT = gql`
-query FetchAnEvent($id: ID!) {
-  event(id: $id) {
-    id
-    eventName
-    description
-    passcode
-    receipts {
+  query FetchAnEvent($id: ID!) {
+    event(id: $id) {
       id
-      name
-      isPaid
-      eventId
-      cardDownId
+      eventName
+      description
+      passcode
+      receipts {
+        id
+        name
+        isPaid
+        eventId
+        cardDownId
+      }
     }
   }
-}
 `
 
 export const GET_ACTIVE_EVENT_RECEIPTS = gql`
-query ActiveEventReceipts($id: ID!){
-  activeEventReceipts(id: $id){  
-  	id
-    name
-    cardDownId
-  	isPaid
+  query ActiveEventReceipts($id: ID) {
+    activeEventReceipts(id: $id) {
+      id
+      name
+      cardDownId
+      isPaid
+    }
   }
- }
- `
+`
 
- export const GET_PAST_EVENT_RECEIPTS = gql`
- query PastEventReceipts($id: ID!){
-  pastEventReceipts(id: $id){  
-  	id
-    name
-    cardDownId
-  	isPaid
+export const GET_PAST_EVENT_RECEIPTS = gql`
+  query PastEventReceipts($id: ID!) {
+    pastEventReceipts(id: $id) {
+      id
+      name
+      cardDownId
+      isPaid
+    }
   }
- }
- `
+`
 
 export const CREATE_EVENT = gql`
   mutation CreateAnEvent($eventName: String, $description: String) {
     addEvent(eventName: $eventName, description: $description) {
-      eventName,
+      eventName
       description
     }
   }
-`;
+`
 
 export const JOIN_EVENT = gql`
   mutation JoinAnEvent($passcode: String) {
     joinEvent(passcode: $passcode) {
       passcode
+    }
   }
-}
-`;
+`
--- app/screens/Account.js
+++ app/screens/Account.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import { Text, SafeAreaView, StyleSheet, View } from "react-native"
 import colors from "../config/colors"
 import { GET_USER } from "../client/queries/userQueries"
 import { useQuery } from "@apollo/client"
+import AppButton from "../components/AppButton"
+import { AuthContext } from "../context/authContext"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
-const Account = () => {
-  const [id, setId] = useState(4)
+const Account = ({ navigation }) => {
+  const { token, setToken } = useContext(AuthContext)
+  const { user, setUser } = useContext(AuthContext)
+  const [id, setId] = useState(user)
   const { loading, error, data } = useQuery(GET_USER, { variables: { id } })
   if (loading) {
     return <Text>Loading</Text>
@@ -18,6 +23,14 @@ const Account = () => {
       <View style={styles.inputContainer}>
         <Text>ACCOUNT</Text>
         <Text>{data.user.email}</Text>
+        <AppButton
+          title="Logout"
+          onPress={async () => {
+            await AsyncStorage.clear()
+            setToken("")
+            setUser("")
+          }}
+        />
       </View>
     </SafeAreaView>
   )
